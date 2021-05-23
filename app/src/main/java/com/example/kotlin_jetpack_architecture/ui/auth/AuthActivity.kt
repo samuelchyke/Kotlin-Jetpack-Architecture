d7b2478b093635ca367345bce19405888457186f
CyloTM <samuelchyke@hotmail.com>
2021-05-23 17:39:34 +0100

    Navigation Graphs for Authentication

---
+++ app/src/main/java/com/example/kotlin_jetpack_architecture/ui/auth/AuthActivity.kt
@@ -0,0 +1,12 @@
+package com.example.kotlin_jetpack_architecture.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.kotlin_jetpack_architecture.R
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
