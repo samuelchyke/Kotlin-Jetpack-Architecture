a68461ad77bd315e04b6280c2822c8321c8d6297
CyloTM <samuelchyke@hotmail.com>
2021-05-25 16:10:14 +0100

    NetworkBoundResource and Coroutines

--- app/src/main/java/com/example/kotlin_jetpack_architecture/util/ErrorHandling.kt
+++ app/src/main/java/com/example/kotlin_jetpack_architecture/util/ErrorHandling.kt
@@ -2,8 +2,19 @@ package com.example.kotlin_jetpack_architecture.util
 
 class ErrorHandling {
 
-    companion object{
+    companion object {
+        const val UNABLE_TO_RESOLVE_HOST = "Unable to resolve host"
+        const val UNABLE_TODO_OPERATION_WO_INTERNET =
+            "Can't do that operation without an internet connection"
+        const val ERROR_CHECK_NETWORK_CONNECTION = "Check network connection."
 
         const val ERROR_UNKNOWN = "Unknown error"
+
+        fun isNetworkError(msg: String): Boolean {
+            return when {
+                msg.contains(UNABLE_TO_RESOLVE_HOST) -> true
+                else -> false
+            }
+        }
     }
 }
